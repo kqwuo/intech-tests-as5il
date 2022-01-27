0a276fce2f1cee4cd6b83a500940cf0c95b8a4ee
Melvin <melvin.delpierre@signature-code.com>
2022-01-27 16:14:28 +0100

    Added test for compteComptableService.getListCompteComptable()

--- comptabilite/src/test/java/com/intech/comptabilite/service/entityservice/CompteComptableServiceTest.java
+++ comptabilite/src/test/java/com/intech/comptabilite/service/entityservice/CompteComptableServiceTest.java
@@ -1,5 +1,8 @@
 package com.intech.comptabilite.service.entityservice;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,11 +10,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.intech.comptabilite.model.CompteComptable;
+import com.intech.comptabilite.repositories.CompteComptableRepository;
 
 @SpringBootTest
 public class CompteComptableServiceTest {
+	
+	@MockBean
+	private CompteComptableRepository repository;
 
 	@Autowired
 	private CompteComptableService compteComptableService;
@@ -28,6 +36,25 @@ public class CompteComptableServiceTest {
 		CompteComptable result = compteComptableService.getByNumero(list, elmToFind.getNumero());
 		
 		Assertions.assertEquals(result, elmToFind);
+	}
+	
+	@Test
+	public void getListCompteComptableTest() {
+		ArrayList<CompteComptable> list = new ArrayList<CompteComptable>();
+		list.add(new CompteComptable(1, "nb1"));
+		list.add(new CompteComptable(2, "nb2"));
+		list.add(new CompteComptable(3, "nb3"));
+		list.add(new CompteComptable(4, "nb4"));
+		when(repository.findAll()).thenReturn(list);
 		
+		List<CompteComptable> result = compteComptableService.getListCompteComptable();
+		
+		Assertions.assertTrue(result.size() == list.size());
+		for(int i = 0; i < result.size(); i++) {
+			Assertions.assertTrue(result.get(i).getLibelle() == list.get(i).getLibelle());
+			Assertions.assertTrue(result.get(i).getNumero() == list.get(i).getNumero());
+		}
+		
+		verify(repository).findAll();
 	}
 }
